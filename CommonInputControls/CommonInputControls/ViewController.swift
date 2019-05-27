b55599108fe705842d6e4ca97832cb43d29500be
Student06 <790659@pdsb.net>
2019-05-27 09:17:15 -0400

    changes to chapter 2.9

--- CommonInputControls/CommonInputControls/ViewController.swift
+++ CommonInputControls/CommonInputControls/ViewController.swift
@@ -10,8 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBAction func ButtonTagged(_ sender: Any) {
+    
+
+    @IBAction func keyboardReturnKeyTapped(_ sender: UITextField) {
+        if let text = sender.text {
+            print(text)
+        }
+    }
+    @IBAction func textChanged(_ sender: UITextField) {
+        if let text = sender.text {
+            print(text)
+        }
+    }
+    @IBAction func ButtonTapped(_ sender: Any) {
         print("Button was pressed.")
+        if toggle.isOn {
+            print("The switch is on.")
+        } else {
+            print("The switch is off.")
+        }
+        print("The slider value is set to \(slider.value)")
     }
     @IBAction func switchToggled(_ sender: UISwitch) {
         if sender.isOn {
@@ -20,6 +38,15 @@ class ViewController: UIViewController {
             print("The switch is off.")
         }
     }
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        print(sender.value)
+    }
+    @IBAction func respondToTapGesture(_ sender: UITapGestureRecognizer) {
+        let location = sender.location(in: view)
+        print(location)
+    }
+    @IBOutlet weak var toggle: UISwitch!
+    @IBOutlet weak var slider: UISlider!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
