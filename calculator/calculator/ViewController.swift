5fe8a8873a7ada19af555da1bdaf3215ce64879b
Student06 <790659@pdsb.net>
2019-06-01 16:01:43 -0400

    calculator lab 2.10

---
+++ calculator/calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Student06 on 2019-05-31.
+//  Copyright © 2019 Student06. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
